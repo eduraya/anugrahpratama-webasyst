ef6f2b12bfb8afe6b6bd259735a35f1e42dfcdf8
bayubmtr <bayubimantoro27@gmail.com>
2022-07-12 22:50:22 +0700

    first commit

---
+++ wa-apps/installer/lib/config/app.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'name'        => 'Installer',
+    'description' => 'Install new apps from the Webasyst Store',
+    'icon'        => 'img/installer.svg',
+    'mobile'      => false,
+    'version'     => '2.6.0',
+    'critical'    => '2.6.0',
+    'system'      => true,
+    'vendor'      => 'webasyst',
+    'csrf'        => true,
+    'ui'          => '1.3,2.0'
+);
---
+++ wa-plugins/payment/tinkoff/lib/config/plugin.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    'name'            => 'Банк Тинькофф',
+    'description'     => 'Оплата картами VISA, MasterCard и Maestro через интернет-эквайринг банка Тинькофф',
+    'icon'            => 'img/tinkoff16.png',
+    'logo'            => 'img/tinkoff.png',
+    'vendor'          => 'webasyst',
+    'version'         => '1.0.20',
+    'type'            => waPayment::TYPE_ONLINE,
+    'partial_refund'  => true,
+    'partial_capture' => true,
+    'fractional_quantity' => true,
+    'stock_units'         => true,
+);
